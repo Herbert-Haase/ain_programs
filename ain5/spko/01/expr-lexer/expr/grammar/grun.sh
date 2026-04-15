68e46f3cb29bd7150aabc5d678887aa8f6bef447
Herbert-Haase <herberthaase02@gmx.de>
2026-04-15 20:01:31 +0200

    add vorzeichen to spko hw2

--- ain5/spko/01/expr-lexer/expr/grammar/grun.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-t="java -cp Expr.jar org.antlr.v4.gui.TestRig grammar.Expr tokens -tokens $1 > ${1}.tokens"
-echo $t
-eval $t
--- ain5/spko/02b/vhdl-expr-ast/expr/ExprToPlantuml.java
+++ ain5/spko/02b/vhdl-expr-ast/expr/ExprToPlantuml.java
@@ -11,12 +11,10 @@ import org.antlr.v4.runtime.tree.ParseTree;
 
 public final class VHDLToAst {
     public static void main(String[] args) throws Exception {
-        // 1. Lexer & Parser initialisieren
+
         VHDLLexer lexer = new VHDLLexer(CharStreams.fromStream(System.in));
         VHDLParser parser = new VHDLParser(new CommonTokenStream(lexer));
 
-        // 2. Parse Tree erzeugen (wir starten beim Statement oder der Expression)
-        // Je nachdem, was du testen willst, z.B. parser.statement()
         ParseTree tree = parser.statement();
 
         if (parser.getNumberOfSyntaxErrors() > 0) {
@@ -24,11 +22,11 @@ public final class VHDLToAst {
             System.exit(1);
         }
 
-        // 3. AST bauen
+        // AST bauen
         VHDLExprBuilder builder = new VHDLExprBuilder();
         Expr ast = builder.build(tree);
 
-        // 4. Test-Ausgabe
+        // Test-Ausgabe
         System.out.println("Generierter AST:");
         System.out.println(new ExprPrinter().print(ast));
     }
